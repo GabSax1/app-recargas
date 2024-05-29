1907abf7d2b5f53e65992a8ad437eee0991e7d9d
Karla Paola Cardozo Ramirez <karlapaolacardozoramirez@MacBook-Pro-de-INFRAESTRUCTURA.local>
2024-05-29 16:50:33 -0600

    componente confirmación recarga

---
+++ src/recarga-completa/recarga-completa.js
@@ -0,0 +1,44 @@
+import { LitElement, html, css } from "lit-element";
+import "../eit-icon";
+
+class RecargaCompleta extends LitElement {
+  static get properties() {
+    return { recargaRealizada: { type: Boolean } };
+  }
+
+  constructor() {
+    super();
+    this.recargaRealizada = false;
+  }
+
+  static get styles() {
+    return css`
+      .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        padding: 20px;
+      }
+    `;
+  }
+  render() {
+    return html`
+      ${this.recargaRealizada
+        ? html`
+            <div class="container">
+              <h2>Recarga realizada correctamente</h2>
+              <eit-icon icon="succefull"></eit-icon>
+            </div>
+          `
+        : html`
+            <div class="container">
+              <h2>Recarga no realizada</h2>
+              <eit-icon icon="cancel"></eit-icon>
+            </div>
+          `}
+    `;
+  }
+}
+
+customElements.define("recarga-completa", RecargaCompleta);
