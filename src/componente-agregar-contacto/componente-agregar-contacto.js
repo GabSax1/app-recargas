421fce048bf98552a34d97262c82e2b06ae4f04a
axlgoze <rbaxel.f@gmail.com>
2024-05-30 19:30:57 -0600

    documentando componente agregar contacto y titulo

--- src/componente-agregar-contacto/componente-agregar-contacto.js
+++ src/componente-agregar-contacto/componente-agregar-contacto.js
@@ -9,13 +9,22 @@ export class ComponenteAgregarContacto extends LitElement {
         `
     ];
 
+    /**
+     * @description `nuevoContacto` es una propiedad de tipo objeto.
+     */
     static get properties() {
         return {
             nuevoContacto: { type: Object },
         };
     }
 
-
+    /**
+     * @description Inicializa la propiedad `nuevoContacto` con valores vacíos para `phone`, `company` y `name`.
+     * @type {Object}
+     * @property {string} phone - Número de teléfono del nuevo contacto, inicializado como una cadena vacía.
+     * @property {string} company - Nombre de la compañía del nuevo contacto, inicializado como una cadena vacía.
+     * @property {string} name - Nombre del nuevo contacto, inicializado como una cadena vacía.
+     */
     constructor() {
         super();
     
@@ -27,28 +36,45 @@ export class ComponenteAgregarContacto extends LitElement {
     
     }
 
-
+    /**
+     * @description Maneja los cambios en los elementos input y actualiza el objeto `nuevoContacto` con los nuevos valores.
+     * @param {Event} e - El objeto de evento que contiene información sobre el evento que se ha producido.
+     * @event input
+    */
     _handleInput(e){
         const { name, value } = e.target;
         this.nuevoContacto = {... this.nuevoContacto, [name]: value};
     }
 
+    /**
+     * @description Maneja la acción de guardar el contacto. Imprime el objeto `nuevoContacto` en la consola y luego limpia los campos del formulario.
+     * @method
+     */
     _handleGuardar(){
         console.log(this.nuevoContacto);
         this._limpiarCampos();
     }
 
+    /**
+     * @description Limpia los campos del formulario reiniciando el objeto `nuevoContacto` a sus valores iniciales vacíos. Fuerza la actualización del DOM.
+     * @method
+     */
     _limpiarCampos(){
         this.nuevoContacto = {
             phone: '',
             company: '',
             name: ''
         };
-        // Para forzar la actualización del DOM
+        
         this.requestUpdate();
     }
 
 
+
+    /**
+     * @description Genera y devuelve una plantilla HTML para el formulario de agregar contacto.
+     * @returns {TemplateResult} Una plantilla HTML que incluye los campos para ingresar los datos del contacto y un botón para guardar.
+     */
     render() {
         return html`
             <div class="agregar">
